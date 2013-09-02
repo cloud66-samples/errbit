255efee6f4e4f2ad482293038cc2b02441d44213
Philip Kallberg <philip@cloud66.com>
2013-09-02 12:42:05 +0100

    update

--- .cloud66/scripts/bundle.sh
+++ .cloud66/scripts/bundle.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source /var/.cloud66_env
 cd $RAILS_STACK_PATH
-bundle exec rake secret > config/initializers/secret_token.rb
+echo "Errbit::Application.config.secret_token = '$(bundle exec rake secret)'" > config/initializers/secret_token.rb
 bundle exec rake db:mongoid:create_indexes
 bundle exec rake db:seed

9ab247a1df1959a26096b8ecb7f5247a9d7e70f8
Philip Kallberg <philip@cloud66.com>
2013-09-02 10:38:44 +0100

    update

--- .cloud66/scripts/bundle.sh
+++ .cloud66/scripts/bundle.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 source /var/.cloud66_env
 cd $RAILS_STACK_PATH
+bundle exec rake secret > config/initializers/secret_token.rb
+bundle exec rake db:mongoid:create_indexes
 bundle exec rake db:seed

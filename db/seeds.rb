71635f4bca84fe4fea5fd3762dc35cf3905a7795
Philip Kallberg <philip@cloud66.com>
2013-09-12 15:24:50 +0100

    update

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,8 @@ puts "Seeding database"
 puts "-------------------------------"
 
 # Create an initial Admin User
-admin_username = "username"
-admin_email = "username@cloud66.com"
+admin_username = "errbit"
+admin_email = "errbit@#{Errbit::Config.host}"
 admin_pass  = 'password'
 
 puts "Creating an initial admin user:"

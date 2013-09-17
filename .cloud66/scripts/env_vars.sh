a891406f334e0d466d540dbfdd5cc5b5d7390371
Philip Kallberg <philip@cloud66.com>
2013-09-17 17:44:43 +0100

    update

--- .cloud66/scripts/env_vars.sh
+++ .cloud66/scripts/env_vars.sh
@@ -3,6 +3,7 @@ cd $RAILS_STACK_PATH
 echo "github_authentication: $GITHUB_AUTHENTICATION
 github_client_id: $GITHUB_CLIENT_ID
 github_secret: $GITHUB_SECRET
+ERRBIT_EMAIL_FROM: $MAILER_SENDER
 host: $ERRBIT_HOST
 email_from: $MAILER_SENDER
 smtp_settings:

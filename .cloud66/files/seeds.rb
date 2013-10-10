14f3a5e815aa464eff8ce1d6934f7ffb6bc43e4c
Philip Kallberg <philip@cloud66.com>
2013-10-10 13:12:39 +0100

    moving everything into .cloud66 folder

--- .cloud66/files/seeds.rb
+++ .cloud66/files/seeds.rb
@@ -3,7 +3,7 @@ puts "-------------------------------"
 
 # Create an initial Admin User
 admin_username = "errbit"
-admin_email = "errbit@#{Errbit::Config.host}"
+admin_email = "errbit@host.com"
 admin_pass  = 'password'
 
 puts "Creating an initial admin user:"
@@ -12,13 +12,13 @@ puts "-- email:    #{admin_email}"
 puts "-- password: #{admin_pass}"
 puts ""
 puts "Be sure to change these credentials ASAP!"
-user = User.find_or_initialize_by(:email => admin_email) do |u|
-  u.name = 'Errbit Admin'
-  u.password = admin_pass
-  u.password_confirmation = admin_pass
-end
+user = User.where(:email => admin_email).first || User.new({
+  :name                   => 'Errbit Admin',
+  :email                  => admin_email,
+  :password               => admin_pass,
+  :password_confirmation  => admin_pass
+})
 user.username = admin_username if Errbit::Config.user_has_username
 
 user.admin = true
 user.save!
-

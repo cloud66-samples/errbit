72feeac1cff5da683815e6563cfbae6847f17564
Philip Kallberg <philip@cloud66.com>
2013-10-10 14:46:22 +0100

    secret token update

--- .cloud66/scripts/bundle.sh
+++ .cloud66/scripts/bundle.sh
@@ -5,9 +5,7 @@ if [ -f $FILE ]
 then
 	echo "File $FILE exists..."
 else
-	source /var/.cloud66_env
-    cd $RAILS_STACK_PATH
-    echo "Errbit::Application.config.secret_token = '$(bundle exec rake secret)'" > config/initializers/secret_token.rb
+    cd $STACK_PATH
     bundle exec rake db:mongoid:create_indexes
     bundle exec rake db:seed
     touch /tmp/bundle_done
